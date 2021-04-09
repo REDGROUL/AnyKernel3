1c133ee271f8d469ea19cc5d8d7cdadd6ddfa377
Najah Muhabbik <72481862+Sixzz9@users.noreply.github.com>
2021-04-09 12:42:44 +0700

    Wahyuu

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Moonlight Kernel (EAS)
+kernel.string=GAME|YOUR|KNTL Kernel (EAS)
 do.devicecheck=1
 do.modules=0
 do.systemless=1
