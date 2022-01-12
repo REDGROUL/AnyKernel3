b6e03f1fbc09d6fde7d836c8888b64be37a3eb74
REDGROUL <38361748+REDGROUL@users.noreply.github.com>
2022-01-12 21:22:17 +0300

    update device for jasmeme

--- anykernel.sh
+++ anykernel.sh
@@ -4,16 +4,16 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Atom-x kernel Xiaomi mi a26x builed REDGROUL (red.d)
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=jasmine_Sprout
+device.name2=wayne
+device.name3=
+device.name4=
 device.name5=
 supported.versions=
 supported.patchlevels=
