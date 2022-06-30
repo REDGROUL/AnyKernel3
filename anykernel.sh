fd0cc42b56fcfd056a5f0ac1aa6d29ec92877eae
REDGROUL <38361748+REDGROUL@users.noreply.github.com>
2022-06-30 20:28:08 +0300

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Atom-x kernel Xiaomi mi a26x builed REDGROUL (red.d)
+kernel.string=Kernel flash
 do.devicecheck=1
 do.modules=0
 do.systemless=1
